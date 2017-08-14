e2d3445bcd7d57e898c2373c28275613caa05262
Binh Robles <bgr5@pge.com>
2017-08-13 23:18:30 -0700

    feat: trying to add directionality smarts

--- colormaze.go
+++ colormaze.go
@@ -10,38 +10,50 @@ func check(e error) {
 	}
 }
 
-type Crumb struct {
+type Point struct {
 	row int
 	col int
 }
 
-func (c Crumb) isNext(m *Maze) bool {
+func (c Point) isNext(m *Maze) bool {
+	// fmt.Printf("location: %v\n", c)
+	// fmt.Printf("searching for: %c\n", m.sequence[m.seqIdx])
+	// fmt.Printf("what I see: %c\n", byte(m.rows[c.row][c.col]))
 	return m.sequence[m.seqIdx] == byte(m.rows[c.row][c.col])
 }
 
 // ensures the crumb won't be thrown over an edge
-func (c Crumb) exists(width int, depth int) bool {
+func (c Point) exists(width int, depth int) bool {
 	return c.row >= 0 && c.row < depth && c.col >= 0 && c.col < width
 }
 
-// return adjacent points north, west, east, then south
-func (c Crumb) getAdjacent(width int, depth int) []Crumb {
-	possible := make([]Crumb, 4)
+// return adjacent points clockwise from last point
+func (c Point) getAdjacent(direction Point, width int, depth int) []Point {
+	possible := make([]Point, 4)
 
-	// north
-	possible[0].row = c.row - 1
-	possible[0].col = c.col
-	// west
-	possible[1].row = c.row
-	possible[1].col = c.col - 1
-	// east
-	possible[2].row = c.row
-	possible[2].col = c.col + 1
-	// south :(
-	possible[3].row = c.row + 1
-	possible[3].col = c.col
+	var strafing Point
+	if direction.row != 0 {
+		strafing.row = 0
+		strafing.col = -1
+	} else {
+		strafing.row = -1
+		strafing.col = 0
+	}
 
-	var actual []Crumb
+	// forward (continue)
+	possible[0].row = c.row + direction.row
+	possible[0].col = c.col + direction.col
+	// left -- no ternaries??
+	possible[1].row = c.row + strafing.row
+	possible[1].col = c.col + strafing.col
+	// right
+	possible[2].row = c.row - strafing.row
+	possible[2].col = c.col - strafing.col
+	// back :(
+	possible[3].row = c.row - direction.row
+	possible[3].col = c.col - direction.col
+
+	var actual []Point
 	for _, p := range possible {
 		if p.exists(width, depth) {
 			actual = append(actual, p)
@@ -55,7 +67,7 @@ type Maze struct {
 	sequence string
 	seqIdx   int
 	rows     []string
-	path     []Crumb // don't initialize this slice, just append to it
+	path     []Point // don't initialize this slice, just append to it
 	width    int
 	depth    int
 }
@@ -94,9 +106,9 @@ func NewMaze(s string) *Maze {
 
 // find entry point (where sequence[0] == rows[len][row])
 // the bottom row (highest index) is the beginning
-func (m *Maze) getEntryPoint() Crumb {
+func (m *Maze) getEntryPoint() Point {
 	entryRow := len(m.rows) - 1
-	var crumb Crumb
+	var crumb Point
 
 	// iterate through every char in the entry row
 	// until we find the char we want
@@ -111,46 +123,65 @@ func (m *Maze) getEntryPoint() Crumb {
 	return crumb
 }
 
-func (m *Maze) getLastCrumb() Crumb {
-	return m.path[len(m.path)-1]
+// returns last crumb and direction we went
+func (m *Maze) getLastPoint() (Point, Point) {
+	last := m.path[len(m.path)-1]
+
+	var direction Point
+	if len(m.path) > 1 {
+		direction.row = m.path[len(m.path)-2].row - last.row
+		direction.row = m.path[len(m.path)-2].col - last.col
+	} else {
+		direction.row = 0
+		direction.col = 0
+	}
+
+	return last, direction
 }
 
-func (m *Maze) goForward() Crumb {
-	var c Crumb
-	last := m.getLastCrumb()
+// find next step (where sequence[n+1] == rows[len-1][row])
+// probably want to prefer forward progress
+func (m *Maze) getNextMove() Point {
+	var c Point
+	last, direction := m.getLastPoint()
 
-	options := last.getAdjacent(m.width, m.depth)
+	options := last.getAdjacent(direction, m.width, m.depth)
 
 	for _, o := range options {
 		if o.isNext(m) {
 			c = o
+			break
 		}
 	}
-
-	fmt.Printf("c: %v\n", c)
 
 	return c
 }
 
 // adds the crumb to the path
 // increments the index of the sequence so we know we good
-func (m *Maze) dropCrumb(c Crumb) {
+func (m *Maze) dropPoint(c Point) {
 	m.path = append(m.path, c)
 	m.seqIdx++
 	m.seqIdx %= len(m.sequence)
-	fmt.Printf("Path forward: %v\n", m.path)
+	fmt.Printf("dropped: %v\n", c)
 }
 
 func main() {
 	maze := NewMaze("maze.txt")
 
 	// find entry point (where sequence[0] == rows[len][row])
-	entry := maze.getEntryPoint()
-	maze.dropCrumb(entry)
+	current := maze.getEntryPoint()
+	maze.dropPoint(current)
 
-	// TODO: find next step (where sequence[n+1] == rows[len-1][row])
-	// probably want to prefer forward progress
-	maze.goForward()
+	// keep moving forward(ish) until we're at the top row
+	iterations := 0
+	for current.row != 0 && iterations < 20 {
+		current := maze.getNextMove()
+		maze.dropPoint(current)
+		iterations++
+	}
+
+	fmt.Println("We're out of the woods!")
 }
 
 // sequence: O G
