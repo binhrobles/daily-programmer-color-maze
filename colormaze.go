beb2462f42c911755286d28984b3bec93d045478
Binh Robles <bgr5@pge.com>
2017-08-13 18:58:23 -0700

    feat: grabbing the entry point

--- colormaze.go
+++ colormaze.go
@@ -10,9 +10,15 @@ func check(e error) {
 	}
 }
 
+type Crumb struct {
+	x int
+	y int
+}
+
 type Maze struct {
 	sequence string
 	rows     []string
+	path     []Crumb // don't initialize this slice, just append to it
 }
 
 // making the assumption that the maze is a square
@@ -26,12 +32,15 @@ func NewMaze(s string) *Maze {
 	sequence := replacer.Replace(raw[0])
 	width := len(replacer.Replace(raw[1]))
 
+	fmt.Println("Maze Specs\n----------")
 	fmt.Printf("width: %d depth: %d\n", width, len(raw)-2)
+	fmt.Printf("sequence: %s\n", sequence)
 
 	rows := make([]string, len(raw)-2)
 
 	for i := 0; i < len(raw)-2; i++ {
 		rows[i] = replacer.Replace(raw[i+1])
+		fmt.Printf("%s\n", raw[i+1])
 	}
 
 	var m Maze
@@ -40,11 +49,33 @@ func NewMaze(s string) *Maze {
 	return &m
 }
 
+// find entry point (where sequence[0] == rows[len][x])
+// the bottom row (highest index) is the beginning
+func getEntryPoint(m *Maze) Crumb {
+	entryRow := len(m.rows) - 1
+	var crumb Crumb
+
+	// iterate through every char in the entry row
+	// until we find the char we want
+	for i, c := range m.rows[entryRow] {
+		if m.sequence[0] == byte(c) {
+			crumb.x = entryRow
+			crumb.y = i
+			break
+		}
+	}
+
+	return crumb
+}
+
 func main() {
 	maze := NewMaze("maze.txt")
-	fmt.Printf("sequence: %s\nmaze: \n%v", maze.sequence, maze.rows)
 
-	// TODO: find entry point (where sequence[0] == rows[len][x])
+	// find entry point (where sequence[0] == rows[len][x])
+	entry := getEntryPoint(maze)
+
+	maze.path = append(maze.path, entry)
+	fmt.Printf("Path forward: %v\n", maze.path)
 
 	// TODO: find next step (where sequence[n+1] == rows[len-1][x])
 	// probably want to prefer forward progress
